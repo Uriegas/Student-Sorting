a29ffba4b3215794793bd49a909093a14930591e
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-08-11 22:30:00 -0500

    App in 2 modes: loop and command line :carousel_horse:

--- src/main/java/com/uriegas/App.java
+++ src/main/java/com/uriegas/App.java
@@ -1,6 +1,7 @@
 package com.uriegas;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * Command line interface for the sorting inscriptions program.
@@ -13,38 +14,61 @@ public class App {
     public static String DEBUG ="\033[95m[DEBUG]\033[0m ";
     public static String SUCCESS ="\033[92m[SUCCESS]\033[0m ";
     public static void main( String[] args ){
-        if( args.length == 1 ){
-            System.out.println( WARNING + "Considering output file as output.csv" );
-            run(args[0], "output.csv");
-            // args[1] = "output.csv";
-        }else if( args.length == 2 ){
-            run(args[0], args[1]);
-        }else{
-            System.out.println( ERROR + "Usage: java -jar gradeording-1.0-SNAPSHOT.jar <input_file> <output_file>");
-            System.out.println( INFO + "1. input_file: absolute or relative path to the excel/csv file with the inscriptions.");
-            System.out.println( INFO + "2. output_file: csv or txt file to append the ordered list.");
-            System.out.println( INFO + "Example: java -jar gradeording-1.0-SNAPSHOT.jar ejemplo1.xlsx output.csv");
-        }
-        // ==> Menu loop
-        // BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        // String line;
-        // try{
-        //     System.out.println("Welcome to the sorting inscriptions program.");
-        //     System.out.println("MENU:");
-        //     System.out.println("1. Sort inscriptions");
-        //     line = in.readLine();
-        //     System.out.println(INFO + "Wrote: " + line);
-        // }catch( IOException e ){
-        //     System.err.println( "Error: " + e.getMessage() );
+        // if( args.length == 1 ){
+        //     System.out.println( WARNING + "Considering output file as output.csv" );
+        //     run(args[0], "output.csv");
+        //     // args[1] = "output.csv";
+        // }else if( args.length == 2 ){
+        //     run(args[0], args[1]);
+        // }else{
+        //     System.out.println( ERROR + "Usage: java -jar gradeording-1.0-SNAPSHOT.jar <input_file> <output_file>");
+        //     System.out.println( INFO + "1. input_file: absolute or relative path to the excel/csv file with the inscriptions.");
+        //     System.out.println( INFO + "2. output_file: csv or txt file to append the ordered list.");
+        //     System.out.println( INFO + "Example: java -jar gradeording-1.0-SNAPSHOT.jar ejemplo1.xlsx output.csv");
         // }
-
-        // while( (line = in.readLine()) != null )
-        //     String[] tokens = line.split(" ");
+        // ==> Menu loop
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        List<Student> students = new ArrayList<Student>();
+        try{
+            System.out.println("Welcome to the sorting inscriptions program.");
+            System.out.println(INFO + "Type 'exit' to exit this program");
+            while(true){
+                System.out.println(INFO + "Type 'view' to view sorted list");
+                System.out.print("Input file please (XLSX or CSV)> ");
+                line = in.readLine();
+                if(line.equals("exit"))
+                    break;
+                else if(line.equals("view")){
+                    if(!students.isEmpty()){
+                        System.out.println(INFO + "Sorted list:");
+                        students.forEach(System.out::println);
+                    }else
+                        System.out.println(ERROR + "List is empty");
+                }else{
+                    System.out.println(INFO + "Reading: " + line + "...");
+                    run(line, "output.csv");
+                    System.out.println(SUCCESS + "Done.");
+                }
+            }
+        }catch( IOException e ){
+            System.err.println( "Error: " + e.getMessage() );
+        }
         // <== Menu loop
     }
-    public static void run(String inputFile, String outputFile){
+    public static List<Student> run(String inputFile, String outputFile){
         System.out.println( INFO + "Executing sorting inscriptions program.");
         System.out.println( DEBUG + "Input file: " + inputFile);
         System.out.println( DEBUG + "Output file: " + outputFile);
+        try{
+            List<Inscription> inscriptions = Reader.read(new File(inputFile));
+            List<Student> students = Sorting.getStudents(inscriptions);
+            students = Sorting.sort(students);
+            Reader.write(new File(outputFile), students);
+            return students;
+        }catch( Exception e ){
+            System.err.println( ERROR + "Error: " + e.getMessage() );
+            return null;
+        }
     }
 }
